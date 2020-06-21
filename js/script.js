036ed62c73eea9ded25c2cd2d3a6c5e729311cb4
dinarrene <dinardavis@gmail.com>
2020-06-21 15:17:47 -0700

    Add colorOptions functions and event listener

--- js/script.js
+++ js/script.js
@@ -1,11 +1,55 @@
 window.onload = function() {
     document.getElementById("name").focus();
-};
+    document.getElementById("other-title").style.display = "none";
+    setColorOptions();
+}
 
 
 
+let selectTheme = document.querySelectorAll("#design > option");
+let colorOptions = document.querySelectorAll("#color > option")
+let defaultColorMenu = document.createElement('option');
 
-document.getElementById("other-title").style.display = "none";
+
+
+function setColorOptions() {
+    if(selectTheme[0].selected){
+        for(let i = 0; i < colorOptions.length; i++){
+            colorOptions[i].hidden = true;
+        }
+    }
+    let colorMenu = document.getElementById('color'); 
+    defaultColorMenu.selected = true;
+    defaultColorMenu.value = 'default';
+    defaultColorMenu.textContent = 'Please Select a T-shirt Theme'
+    colorMenu.insertBefore(defaultColorMenu, colorMenu.childNodes[1]);
+}
+
+
+
+function changeColorOptions() {
+    if(selectTheme[1].selected){
+        setColorOptions();
+        for(let i = 0; i < colorOptions.length; i++){
+            colorOptions[i].hidden = false;
+            for(let i = 3; i < colorOptions.length; i++){
+                colorOptions[i].hidden = true;
+            }
+        }
+    } else if(selectTheme[2].selected){
+        setColorOptions();
+        for(let i = 0; i < colorOptions.length; i++){
+            colorOptions[i].hidden = true;
+            for(let i = 3; i < colorOptions.length; i++){
+                colorOptions[i].hidden = false;
+            }
+        }
+    } else {
+        setColorOptions();
+    }
+}
+
+
 
 title.addEventListener('change', () => {
     if(document.getElementById("title").value == "other"){
@@ -14,6 +58,12 @@ title.addEventListener('change', () => {
         document.getElementById("other-title").style.display = "none";
     }
 })
+
+design.addEventListener('change', () => {
+    changeColorOptions();
+})
+
+
 
 
 
